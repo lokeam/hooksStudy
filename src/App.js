7df869ccb3a6e573f7cd04ec68b4d357e414a314
Ahnming Loke <lokeahnming@gmail.com>
2019-11-27 19:51:19 -0500

    feat: add more edge, use cases + gotchas for useState

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
 
   // Second example: Incrementing a counter
   const [ count, setCount ] = useState(10);
+  const [ {countExampleA, countExampleB}, setCountExample ] = useState({countExampleA: 10, countExampleB: 15});
   return (
     <div className="App">
       <header className="App-header">
@@ -28,17 +29,32 @@ const App = () => {
       </header>
       <section className="area__useState">
         <div className="hero__copy">
-        <h2>useState</h2>
-        <div className="example">
-          <h3>Example 1: Expensive Operations</h3>
-          <p>Setting a function as the second parameter within the useState hook can be called within an arrow function in order to ensure that an operation is only called once</p>
-        </div>
-        <div className="example">
-        <h3>Example 2: Incrementing a Counter</h3>
-          <button onClick={ () => setCount(count + 1)}>+</button>
-          <div>{count}</div>
-        </div>
-
+          <h2>useState</h2>
+          <div className="example">
+            <h3>Example 1: Expensive Operations</h3>
+            <p>Setting a function as the second parameter within the useState hook can be called within an arrow function in order to ensure that an operation is only called once</p>
+          </div>
+          <div className="example">
+            <h3>Example 2: Incrementing a Counter</h3>
+            <button onClick={ () => setCount(count + 1)}>+</button>
+            <div>{count}</div>
+          </div>
+          <div className="example">
+            <h3>Example 3: Incrementing a Counter</h3>
+            <p>Using an updater function (currentCount) that accepts a single parameter.</p>
+            <p>This menthod is useful when you <want></want> to avoid race conditions when more than one update occurs simultaneously said update may get overwritten.</p>
+            <p>Note: When updating state using this example, you must use a spread operator or manually call out the other parts of the state you want to save,
+                if you want to keep any of the other values</p>
+            <button
+              onClick={ () =>
+                setCountExample(currentState => ({
+                  ...currentState,
+                  countExampleA: currentState.countExampleA + 1
+                }))
+              }>+</button>
+            <div className="countExampleA">Example 3 count A: {countExampleA}</div>
+            <div className="countExampleB">Example 3 count B: {countExampleB}</div>
+          </div>
         </div>
       </section>
     </div>
